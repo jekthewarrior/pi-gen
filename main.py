3b345c5effb991f9bfaaa4cadc3c7fda24ea15be
jekthewarrior <pranav16@outlook.com>
2020-05-29 17:14:45 -0700

    Simple implementation with only name and gender

--- main.py
+++ main.py
@@ -5,26 +5,29 @@ import Generators.nameAndGender as ng
 import Database.datasetmanager as dsm
 
 # Here lies the Private Information Generator
+while(not ans.lower().startswith('y')):
+	# Set-up templates and field selection
 
-# Set-up templates and field selection
+	# Ask for number of people to generate info for
+	num = input("Please enter how many people to generate information for: ")
 
-# Ask for number of people to generate info for
-num = input("Please enter how many people to generate information for: ")
+	# Run database check before calling other functions
+	numFirst = dsm.numFirsts()
+	numLast = dsm.numLasts()
 
-# Run database check before calling other functions
-numFirst = dsm.numFirsts()
-numLast = dsm.numLasts()
+	# Call generators based on selected template and write data to JSON file/git c
+	with open("PI.json", "w") as outfile:
 
-# Call generators based on selected template and write data to JSON file
+		outfile.write("[")
 
-with open("PI.json", "w") as outfile:
+		for i in range(0, num):
 
-	outfile.write("[")
+			#
+			outfile.write(json.dumps(ng.nameAndGender(numFirst, numLast), indent=4))
+			if(i != num - 1):
+				outfile.write(",\n")
 
-	for i in range(0, num):
+		outfile.write("]")
 
-		outfile.write(json.dumps(ng.nameAndGender(numFirst, numLast), indent=4))
-		if(i != num - 1):
-			outfile.write(",\n")
-
-	outfile.write("]")
+	# Prompt user to quit after each file generation
+	ans = input("Do you want to quit?")
