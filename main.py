17a046d6ba925c405fc3d51b39cd96e3e2f64db6
jekthewarrior <pranav16@outlook.com>
2020-05-31 16:17:23 -0700

    Implemented database check option to reduce runtime in consecutive runs

--- main.py
+++ main.py
@@ -10,14 +10,23 @@ ans = ""
 while(not ans.lower().startswith('y')):
 	# Set-up templates and field selection
 
+	# Ask to run database check before calling other functions. Otherwise just use variables from the config file
+	bop = input("Do you want to run a database check?")
+
+	if(bop.lower().startswith('y')):
+		# Perform database check
+		numFirst = dsm.numFirsts()
+		numLast = dsm.numLasts()
+	else:
+		# Retrieve previous values from config file
+		with open("Database/config.txt", 'r') as infile:
+			numFirst = int(infile.readline().strip("\n"))
+			numLast = int(infile.readline().strip("\n"))
+
 	# Ask for number of people to generate info for
 	num = int(input("Please enter how many people to generate information for: "))
 
-	# Run database check before calling other functions
-	numFirst = dsm.numFirsts()
-	numLast = dsm.numLasts()
-
-	# Call generators based on selected template and write data to JSON file/git c
+	# Call generators based on selected template and write data to JSON file
 	with open("PI.json", "w") as outfile:
 
 		outfile.write("[")
@@ -33,3 +42,8 @@ while(not ans.lower().startswith('y')):
 
 	# Prompt user to quit after each file generation
 	ans = input("Do you want to quit?")
+
+# Update values in config file, should the database checks have occurred
+with open("Database/config.txt", "w") as outfile:
+	outfile.write(str(numFirst) + "\n")
+	outfile.write(str(numLast) + "")
