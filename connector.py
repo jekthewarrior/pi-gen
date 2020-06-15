4d2eb7d87c1c84f7fda3b2f4f1717da695509b46
jekthewarrior <pranav16@outlook.com>
2020-06-14 21:35:26 -0700

    Changed function docstring

--- connector.py
+++ connector.py
@@ -1,7 +1,8 @@
 # Imports go here
 import kafka as kf
 
-def createKafkaProducer():
-	"""Creating the Kafka producer to interact with the Kafka cluster"""
+def kafkaConnector(**configOptions):
+	"""A menu to select and connect to Kafka brokers and have an easy menu to conveniently send
+	records as a producer to whatever topic and partition needed"""
 	return kf.KafkaProducer()
 
